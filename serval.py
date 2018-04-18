ef94144aebc3a2f352604088b229a0cf31e01934
icetiger nate <icetiger@nate.com>
2018-04-18 11:07:39 +0900

    Update serval.py
    test  commit by ice

--- serval.py
+++ serval.py
@@ -18,6 +18,7 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
+ *                                                                         *
  ***************************************************************************/
 """
 from PyQt4 import QtGui, uic
